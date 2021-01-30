6136f619d473e54d0bd44bf06622642e7fc91ee1
Maor Kamilyan <MaorKamilyan@gmail.com>
2021-01-30 11:45:42 +0200

    refactor code

--- server/app.js
+++ server/app.js
@@ -55,10 +55,10 @@ app.use(function (err, req, res, next) {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/../client/build')))
+  app.use(express.static(path.join(path.resolve(), '/client/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '/../client', 'build', 'index.html'))
+    res.sendFile(path.resolve(path.resolve(), 'client', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
